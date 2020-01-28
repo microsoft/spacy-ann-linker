ee7a3d4fc5ddb5839221730c771623eef7099d5c
Kabir Khan <kakh@microsoft.com>
2020-01-28 11:51:29 -0800

    Deployed 94a3af2 with MkDocs version: 1.0.4

---
+++ src/parameter_types/path/tutorial001.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import typer
+
+
+def main(config: Path = typer.Option(None)):
+    if config is None:
+        typer.echo("No config file")
+        raise typer.Abort()
+    if config.is_file():
+        text = config.read_text()
+        typer.echo(f"Config file contents: {text}")
+    elif config.is_dir():
+        typer.echo("Config is a directory, will use all its config files")
+    elif not config.exists():
+        typer.echo("The config doesn't exist")
+
+
+if __name__ == "__main__":
+    typer.run(main)
