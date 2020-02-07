7dfd9715718f7a0c9a1a7c5578a1e23d9ba1b176
Kabir Khan <kakh@microsoft.com>
2020-02-07 15:01:13 -0800

    fix tests and lint modules in scripts

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,4 +5,4 @@ set -x
 
 mypy spacy_ann --disallow-untyped-defs
 black spacy_ann tests --check
-isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --thirdparty typer typer tests
+isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --thirdparty spacy_ann spacy_ann tests
--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,7 @@ set -x
 
 export PYTHONPATH=./docs/src
 # Use xdist-pytest --forked to ensure modified sys.path to import relative modules in examples keeps working
-pytest --cov=typer --cov=tests --cov=docs/src --cov-report=term-missing -o console_output_style=progress --forked --numprocesses=auto ${@}
+pytest --cov=spacy_ann --cov=tests --cov-report=term-missing -o console_output_style=progress --forked --numprocesses=auto ${@}
 bash ./scripts/lint.sh
 # Include tests for files
 bash ./scripts/test-files.sh
