ad594d01c2c33e8fe35018c0d5ac0d032828f8f9
Kabir Khan <kakh@microsoft.com>
2020-02-23 22:38:18 -0800

    bump version

--- spacy_ann/__init__.py
+++ spacy_ann/__init__.py
@@ -3,7 +3,7 @@
 """spaCy ANN Linker, a pipeline component for generating spaCy KnowledgeBase Alias Candidates for Entity Linking."""
 
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from .ann_linker import AnnLinker
 from .remote_ann_linker import RemoteAnnLinker
