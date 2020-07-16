11a2a997d36555b12abea0109cc4fa89aad79e0d
Kabir Khan <kakh@microsoft.com>
2020-07-16 15:45:32 -0700

    upgrade version to 0.3.0

--- spacy_ann/__init__.py
+++ spacy_ann/__init__.py
@@ -3,21 +3,7 @@
 """spaCy ANN Linker, a pipeline component for generating spaCy KnowledgeBase Alias Candidates for Entity Linking."""
 
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from .ann_linker import AnnLinker
 from .remote_ann_linker import RemoteAnnLinker
-
-# TODO: Uncomment (and probably fix a bit) once this PR is merged upstream
-# https://github.com/explosion/spaCy/pull/4988 to enable kb registry with
-# customizable `get_candidates` function
-#
-# from spacy.kb import KnowledgeBase
-# from spacy.tokens import Span
-# from spacy.util import registry
-
-
-# @registry.kb.register("get_candidates")
-# def get_candidates(kb: KnowledgeBase, ent: Span):
-#     alias = ent._.alias_candidates[0] if ent._.alias_candidates else ent.text
-#     return kb.get_candidates(alias)
