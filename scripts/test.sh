4351c6ee399118719e31043601541531d2cddf55
Kabir <kakh@microsoft.com>
2020-11-18 17:41:22 -0800

    use list for json serialization

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,7 @@ set -x
 
 export PYTHONPATH=./docs/src
 # Use xdist-pytest --forked to ensure modified sys.path to import relative modules in examples keeps working
-pytest --cov=spacy_ann --cov-report=term-missing -o console_output_style=progress
+pytest tests/test_ann_kb.py --cov=spacy_ann.ann_kb --cov-report=term-missing -o console_output_style=progress
 # bash ./scripts/lint.sh
 # Include tests for files
 bash ./scripts/test-files.sh
